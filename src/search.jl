29f3debf9f952fd9816b824b0da51bd51503cbdb
WhiffleFish <tylertronics@gmail.com>
2023-12-12 20:55:31 -0700

    accomodate singleton vector input for admissible cost

--- src/search.jl
+++ src/search.jl
@@ -1,16 +1,17 @@
-function POMDPTools.action_info(planner::WorstCaseForwardSearchPlanner, b, d)
+function POMDPTools.action_info(planner::WorstCaseForwardSearchPlanner, b, d::Real)
     t0 = time()
     (;sol, cpomdp, tree) = planner
     (;max_time, max_iter) = sol
-    set_root!(tree, planner.cpomdp, b, d)
+    set_root!(tree, cpomdp, b, d)
     iter = 0
     while (time() - t0 < max_time) && (iter < max_iter)
         expand!(tree, sol)
         iter += 1
     end
     a_idx, Qa = best_action_and_value(tree)
-    a = ordered_actions(planner.cpomdp)[a_idx]
+    a = ordered_actions(cpomdp)[a_idx]
     return a, (; V=Qa, tree)
 end
 
-POMDPs.action(planner::WorstCaseForwardSearchPlanner, b, d) = first(action_info(planner, b, d))
+POMDPs.action(planner::WorstCaseForwardSearchPlanner, b, d::Real) = first(action_info(planner, b, d))
+POMDPs.action(planner::WorstCaseForwardSearchPlanner, b, d::AbstractVector) = first(action_info(planner, b, only(d)))
